c3def75f245bd3b495c0bd75d4ccd82405d7e8de
laura.chacon <laurachacon92@gmail.com>
2016-08-02 17:05:42 +0200

    Change to integer the score values of the history

--- controllers/get_action_history_controller.py
+++ controllers/get_action_history_controller.py
@@ -9,6 +9,10 @@ import datetime
 
 class GetActionHistoryController(object):
     def on_get(self, req, resp, uid):
+        def f(x):
+            x['score'] = int(x['score'])
+            return x
         user_history = model.user.read_history(uid)
+        user_history = map(f, user_history)
         req.context['result'] = {'user_history': user_history}
         resp.status = falcon.HTTP_200
