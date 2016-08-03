44ca5d0e751099bad09aedf6a5e9242cdf954c80
laura.chacon <laurachacon92@gmail.com>
2016-08-03 17:53:07 +0200

    change format to write dates in dynamodb

--- controllers/create_action_controller.py
+++ controllers/create_action_controller.py
@@ -22,6 +22,7 @@ class CreateActionController(object):
         action_type = req.context['body'].get('action_type')
         score = req.context['body'].get('score')
         date = json.dumps(datetime.date.today(), default=date_handler)
+	date = str(datetime.datetime.today()).split()[0]
         action = Action(uid=uid, action_id=action_id, action_type=action_type,
                         datetime=date, section=section, score=score)
         action.write()
